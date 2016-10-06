202095b7ce6adc4cacc9c924626249c7fb0e484e
Caryn Ligon <carynligon@gmail.com>
2016-10-06 12:12:30 -0500

    made a simple character counter with vanilla

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-console.dir(document.getElementById('container'));
+var container = document.getElementById('container');
 
-document.addEventListener('scroll', function(e) {
-  if (e.target.scrollingElement.scrollTop > 100) {
-    console.log('scrolled!');
-    document.getElementById('container').textContent = 'hi';
-  }
+var header = document.createElement('header');
+header.textContent = "Character Counter";
+container.appendChild(header);
+
+console.dir(container);
+
+var textForm = document.createElement('form');
+var textInput = document.createElement('textarea');
+var resetText = document.createElement('input');
+container.appendChild(textForm);
+textForm.appendChild(textInput);
+textForm.appendChild(resetText);
+textInput.setAttribute('placeholder', 'type something');
+resetText.setAttribute('type', 'reset');
+var textLength = 0;
+
+textInput.addEventListener('keyup', function(e) {
+  e.preventDefault();
+  console.dir(textInput);
+  textLength = textInput.value.length;
+  characters.textContent = textLength + ' characters';
 });
+
+resetText.addEventListener('click', function(e) {
+  characters.textContent = '0 characters';
+});
+
+var characters = document.createElement('div');
+container.appendChild(characters);
+characters.textContent = '0 characters';
