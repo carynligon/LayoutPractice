8bbff1f581e887126f3fb4e32de616db7cf182d3
Caryn Ligon <carynligon@gmail.com>
2016-10-08 12:24:34 -0500

    added some flare to the letter

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var $header = $('<header></header>');
 var container = $(document).find('#container');
 var $h1 = $('<h1><span>I\'m</span> with <span>Her</span></h1>');
-var $marquee = $('<h1 id="marquee-container"><span>"I’ve always believed you can learn something from nearly everybody you meet, if you’re open to it."</span></h1>')
+var $marquee = $('<h1 id="marquee-container"><span>"I’ve always believed you can learn something from nearly everybody you meet, if you’re open to it."</span></h1>');
+var $marginalized = $('<div class="marginalized-container"><p>Let\'s continue to stand up for those who are vulnerable to being left out or <span class="blink">marginalized</span>.</p></div>')
 function addStars() {
   $header.empty();
   i = 0;
@@ -17,14 +18,15 @@ function spinStars(e) {
   $(document).find('.star-div').find('i').css('transform', 'rotate(' + (startAngle + e.target.scrollingElement.scrollTop) + 'deg)');
   if (e.target.scrollingElement.scrollTop === 0) {
     $(document).find('.letter-div').css('transform', 'rotate(0deg)');
-  } else {
+  } else if (e.target.scrollingElement.scrollTop >= 440 ){
     $(document).find('.letter-div').css('transform', 'rotate(-10deg)');
   }
 }
-var $letter = $('<div class="letter-div-wrapper"><h3>RE: JOB POSITION - COMMANDER IN CHIEF</h3><div class="letter-div"><p>Plaid kinfolk slow-carb swag bespoke asymmetrical. Godard artisan fap, blue bottle neutra polaroid stumptown hoodie four loko selfies marfa freegan tacos blog disrupt. Williamsburg VHS af, air plant knausgaard locavore mlkshk bicycle rights edison bulb pug typewriter meditation sustainable venmo hella. Tofu typewriter 3 wolf moon pug. Freegan helvetica thundercats, kale chips heirloom fap leggings master cleanse direct trade vaporware. Selvage fanny pack scenester, selfies neutra four dollar toast humblebrag yr put a bird on it hexagon umami kinfolk. Polaroid shoreditch hashtag beard gochujang, heirloom la croix sartorial.</p><p>Retro plaid venmo, scenester coloring book freegan umami ramps literally air plant. 8-bit waistcoat biodiesel, authentic deep v mustache migas next level knausgaard. Pug intelligentsia street art, ethical celiac mlkshk flannel subway tile echo park. Iceland mlkshk pabst biodiesel flannel fanny pack. Freegan subway tile cardigan paleo knausgaard, wayfarers truffaut tilde narwhal food truck kombucha coloring book. Raclette PBR&B next level wayfarers lo-fi cronut chillwave. Heirloom tilde truffaut chicharrones readymade.</p><p>Ennui pop-up sustainable, celiac vice lumbersexual meggings single-origin coffee whatever meditation cronut fingerstache ethical. Kinfolk irony PBR&B street art activated charcoal unicorn. Whatever meditation normcore, aesthetic +1 fixie subway tile snackwave cred craft beer. Etsy chillwave fap, 90\'s blog bicycle rights cardigan tofu paleo jianbing. Bitters forage roof party semiotics williamsburg 90\'s. Tumblr pabst venmo kogi butcher 90\'s, literally pour-over kickstarter before they sold out. Put a bird on it hoodie austin subway tile vinyl.</p><p>Irony crucifix plaid truffaut stumptown hexagon lumbersexual. Thundercats occupy offal air plant. Jianbing wolf microdosing, normcore try-hard kogi slow-carb ennui leggings occupy sartorial pickled 8-bit gluten-free. Tbh chicharrones freegan, lomo twee PBR&B gentrify blue bottle ethical franzen DIY bushwick. Retro pok pok stumptown, franzen skateboard seitan man bun poutine knausgaard post-ironic. Meditation cliche williamsburg try-hard gastropub cold-pressed fashion axe taxidermy, man braid kitsch hell of. Bicycle rights cardigan roof party, skateboard literally whatever kinfolk humblebrag you probably haven\'t heard of them ennui.</p></div></div>')
+var $letter = $('<div class="letter-div-wrapper"><h3>RE: LILLY (SEVEN-YEAR-OLD) - NEXT PRESIDENT</h3><div class="letter-div"><ul class="address-info"><li>10 September 2016</li><br><li>United States of America</li><li>12345 Lake Ave.</li><li>Chappaqua, NY 10514</li></ul><br><p>Dear Lilly:</p><br><p>I received a terrific message from your mom, and was touched to learn that you want to change your name to "Lillary" so that you can be president when you grow up. I think Lilly is a great name -- and I hope you know that you don\'t have to change it to become president; if you dream big, work hard, and care deeply about what you believe in, there\'s no limit to what you can achieve.</p><p>I know that it may seem like a lot now, but as you grow and learn and search for your own place in the world, I hope you\'ll consider how you can make your voice heard. Speak your mind in your classes, and at meetings once you have a job. Proudly take credit for your ideas. Have confidence in the value of your contributions. And if the space you\'re in doesn\'t have room for your voice, don\'t be afraid to carve out a space of your own. Don\'t be discouraged. Don\'t give in. Don\'t give up. Don\'t quit -- on yourself, on your dreams, on your future. You really can be anything you want to be. I may become the first woman president, but you, Lilly, could be next.</p><br><p>With warm wishes, I am</p><p>Sincerely yours,</p><img src="http://ih0.redbubble.net/image.175314063.2962/flat,800x800,075,f-c,0,75,800,331.u2.jpg"/></div></div>')
 container.append($header);
 container.append($letter);
 addStars();
 $(document).on('scroll', spinStars);
 $(window).on('resize', addStars);
 container.append($marquee);
+container.append($marginalized);
